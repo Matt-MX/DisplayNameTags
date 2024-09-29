677c127ea76fa8c61bfd771907d655f210956828
MattMX <mangrovethefox@outlook.com>
2024-09-29 16:19:29 +0100

    :bug: Refresh tags automatically when command is ran

--- src/main/java/com/mattmx/nametags/NameTagsCommand.java
+++ src/main/java/com/mattmx/nametags/NameTagsCommand.java
@@ -1,10 +1,16 @@
 package com.mattmx.nametags;
 
+import com.mattmx.nametags.entity.NameTagEntity;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class NameTagsCommand implements CommandExecutor {
     private final @NotNull NameTags plugin;
@@ -16,7 +22,24 @@ public class NameTagsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         this.plugin.reloadConfig();
-        sender.sendMessage(Component.text("Reloaded!"));
+
+        for (final Player player : Bukkit.getOnlinePlayers()) {
+            final NameTagEntity tag = plugin.getEntityManager().removeEntity(player);
+
+            if (tag == null) continue;
+
+            tag.destroy();
+
+            final NameTagEntity newTag = plugin.getEntityManager().getOrCreateNameTagEntity(player);
+
+            for (final UUID viewer : tag.getPassenger().getViewers()) {
+                newTag.getPassenger().addViewer(viewer);
+            }
+
+            newTag.updateVisibility();
+        }
+
+        sender.sendMessage(Component.text("Reloaded!").color(NamedTextColor.GREEN));
         return false;
     }
 }
