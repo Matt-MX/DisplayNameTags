e4a3f70d9852ffcff9e942a758a48b7ca53a28b5
MattMX <mangrovethefox@outlook.com>
2025-05-12 16:00:21 +0100

    Allow creation on any thread

--- src/main/java/com/mattmx/nametags/event/NameTagEntityCreateEvent.java
+++ src/main/java/com/mattmx/nametags/event/NameTagEntityCreateEvent.java
@@ -1,6 +1,7 @@
 package com.mattmx.nametags.event;
 
 import com.mattmx.nametags.entity.NameTagEntity;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -10,6 +11,8 @@ public class NameTagEntityCreateEvent extends Event {
     private final @NotNull NameTagEntity nameTag;
 
     public NameTagEntityCreateEvent(@NotNull NameTagEntity nameTag) {
+        super(!Bukkit.isPrimaryThread());
+
         this.nameTag = nameTag;
     }
 
